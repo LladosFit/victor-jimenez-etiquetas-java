ad4ef43cd174c00d584e7cc1a4c8d3f019315c58
Victor <victortwt6@gmail.com>
2024-10-17 10:44:56 +0200

    Un nuevo commit con clase main completa

--- Main.java
+++ Main.java
@@ -1,1 +1,9 @@
-Holaaaaaaaaaa
+public class MAin {
+	public satatic void main(String[] args){
+		if(args.lenght == 0){
+			System.out.println("No hay argumentos");
+		}else{
+			System.out.println("Hay " + arg.lenght() + " argumentos");
+		}
+	}
+}
